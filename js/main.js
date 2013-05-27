1101deb92dcf8e76a9e9f449b1ba0e93529d4884
Kristy Miller <miller.kristy06@gmail.com>
2013-05-26 22:21:04 -0400

    [#50577915] - Global navigation for logged in user updated with new notification icon, notification bubble, user profile image and username.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,15 @@
 
 (function($){
 
+
+	/* In the global navigation, when a user hovers over their profile image and username
+	it will display a dropdown menu with links to their profile, dashboard, settings and
+	logout */
+	
+	$(".user-dd").hover(function(e){
+		var userDDMenu = $(".user-dd-menu");
+		userDDMenu.css("display", "block");
+	});
 		
 	
 })(jQuery); // end private scope

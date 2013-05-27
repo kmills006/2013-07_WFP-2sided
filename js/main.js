53ddb299a1c0782b6490b14b79282ae8af439cd4
Kristy Miller <miller.kristy06@gmail.com>
2013-05-27 13:53:29 -0400

    (#5058175700 - Tab navigation fixed on user dashboard.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@
 
 (function($){
 
-
 	/* Global Navigation
 	--------------------
 	--------------------
@@ -32,67 +31,111 @@
 	------------------
 	----------------*/
 	
-	/* On the user dashboard, tabs are used on the left side to control
-	what information is being displayed */
-	$( "#tabs" ).tabs();
+	if(window.location.pathname == "/wfp_2sided/user_dashboard.html"){
+		/* On the user dashboard, tabs are used on the left side to control
+		what information is being displayed */
+		$( "#tabs" ).tabs();
+
+		// Setting the global navigation tab icons
+		$(".ud-tabs li a").each(function(i){
+			var udTabItem = $(this);
+			
+			switch(udTabItem.text()){
+				case "Study":
+					udTabItem.css("background-image", "url(img/icons/active_user_globe.png)");
+					udTabItem.css("background-size", "36px 45px");
+					break;
+					
+				case "Notifications":
+					udTabItem.css("background-image", "url(img/icons/notifications.png)");
+					udTabItem.css("background-size", "35px 25px");
+					break;
+					
+				case "Achievements":
+					udTabItem.css("background-image", "url(img/icons/achievement.png)");
+					udTabItem.css("background-size", "35px 44px");
+					break;
+					
+				case "Settings":
+					udTabItem.css("background-image", "url(img/icons/settings.png)");
+					udTabItem.css("background-size", "50px 35px");
+					break;
+					
+			} // End of switch
+		});
 	
-	/* Changing the active state on the user dashboard tag navigation */
-	$(".ud-tabs li").on("click", function(e){
 		
-		// Removing the active icon
-		var activeTabIcon = $(".ud-tab-active img");
-		switch(activeTabIcon.attr("src")){
-			case "img/icons/active_user_globe.png":
-				activeTabIcon.attr("src", "img/icons/user_globe.png");
-				
-				break;
-			case "img/icons/active_notifications.png":
-				activeTabIcon.attr("src", "img/icons/notifications.png");
+		// User dashboard tab click
+		$(".ud-tabs li").on("click", function(e){
 
-				break;
-			case "img/icons/active_achievement.png":
-				activeTabIcon.attr("src", "img/icons/achievement.png");
-				break;
-				
-			case "img/icons/active_settings.png":
-				activeTabIcon.attr("src", "img/icons/settings.png");
-				break;
-		}
+			console.log($(".ud-tab-active a"));
+			
+			// Removing the active class
+			var prevActiveTab = $(".ud-tab-active a");
+			switch(prevActiveTab.text()){
+				case "Study":
+					prevActiveTab.css("background-image", "url(img/icons/user_globe.png)");
+					prevActiveTab.css("background-size", "36px 45px");
+					break;
+					
+				case "Notifications":
+					prevActiveTab.css("background-image", "url(img/icons/notifications.png)");
+					prevActiveTab.css("background-size", "35px 25px");
+					break;
+					
+				case "Achievements":
+					prevActiveTab.css("background-image", "url(img/icons/achievement.png)");
+					prevActiveTab.css("background-size", "35px 44px");
+					break;
+					
+				case "Settings":
+					prevActiveTab.css("background-image", "url(img/icons/settings.png)");
+					prevActiveTab.css("background-size", "50px 35px");
+					break;
+					
+			} // End of switch
+			
+			$(".ud-tab-active").removeClass("ud-tab-active");
+	
+	
 		
-		// Removing the active class
-		$(".ud-tab-active").removeClass("ud-tab-active");
-
-
-
-		// Adding the active class to the clicked tab
-		$(this).attr("class", "ud-tab-active");
-		
-		// Setting the new active icon
-		var newActiveIcon = $(".ud-tab-active img");
-		switch(newActiveIcon.attr("src")){
-			case "img/icons/user_globe.png":
-				newActiveIcon.attr("src", "img/icons/active_user_globe.png");
+			// Adding the active class to the clicked tab
+			$(this).attr("class", "ud-tab-active")
+			
+			var newActiveTab = $(".ud-tab-active a");
+			// Setting the new active icon
+			switch(newActiveTab.text()){
+				case "Study":
+					newActiveTab.css("background-image", "url(img/icons/active_user_globe.png)");
+					newActiveTab.css("background-size", "36px 45px");
 				
-				break;
-			case "img/icons/notifications.png":
-				newActiveIcon.attr("src", "img/icons/active_notifications.png");
-				
-				break;
-			case "img/icons/achievement.png":
-				newActiveIcon.attr("src", "img/icons/active_achievement.png");
-				
-				break;
-				
-			case "img/icons/settings.png":
-				newActiveIcon.attr("src", "img/icons/active_settings.png");
-				
-				break;
-		}
-
-
-
-		
-	}); // End of user dashboard tab click
+					break;
+				case "Notifications":
+					newActiveTab.css("background-image", "url(img/icons/active_notifications.png)");
+					newActiveTab.css("background-size", "35px 25px");
+					
+					break;
+				case "Achievements":
+					console.log("hereere");
+					
+					console.log(newActiveTab);
+					newActiveTab.css("background-image", "url(img/icons/active_achievement.png)");
+					newActiveTab.css("background-size", "35px 44px");
+					
+					break;
+					
+				case "Settings":
+					newActiveTab.css("background-image", "url(img/icons/active_settings.png)");
+					newActiveTab.css("background-size", "50px 35px");
+					
+					break;
+			};
+	
+	
+	
+			
+		}); // End of user dashboard tab
+	} 
 	
 	
 	

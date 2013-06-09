1f655090a4146a8c4471c34661e09b51a54245d0
Kristy Miller <miller.kristy06@gmail.com>
2013-06-08 21:09:06 -0400

    Composer updated

---
+++ fuel/app/logs/2013/06/09.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2013-06-09 03:08:17 --> Error - ViewModel "\View_Landing_404" could not be found. in /Applications/MAMP/htdocs/2sided/fuel/core/classes/viewmodel.php on line 70

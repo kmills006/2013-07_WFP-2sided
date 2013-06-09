e1028f5307e2c1e3f1a92217c8dc2fde3dc2101d
Kristy Miller <miller.kristy06@gmail.com>
2013-06-08 23:16:18 -0400

    [#51249815] - Landing page displaying with images; Login controller created

--- fuel/app/classes/controller/login.php
+++ fuel/app/classes/controller/login.php
@@ -1,13 +1,13 @@
 <?php
 
-class Controller_Login extends Controller_Template
-{
+class Controller_Login extends Controller{
 
-	public function action_index()
-	{
+	public function action_index(){
+/*
 		$data["subnav"] = array('index'=> 'active' );
 		$this->template->title = 'Login &raquo; Index';
 		$this->template->content = View::forge('login/index', $data);
+*/
 	}
 
 }

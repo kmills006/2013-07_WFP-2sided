31991b9261aee5064687b337db5d01786d92982a
Kristy Miller <miller.kristy06@gmail.com>
2013-06-11 14:23:15 -0400

    [#51368563] - Dropdown menu on logged in global navigation not working correctly when using anchor tags.

--- fuel/app/views/includes/logged_in/header.php
+++ fuel/app/views/includes/logged_in/header.php
@@ -8,20 +8,18 @@
 	    			</form>
 	    			
 	    			<!-- Profile picture and username dropdown -->
-<!--
 	    			<a class="globe-nav user-dd" href="#"><? echo Asset::img('profile_placeholders/kmills006_placeholder.png'); ?>kmills006
 	    			
 		    			<ul class="dd-menu">
-		    				<li><? echo Html::anchor('profile', 'View Profile'); ?></li>
-		    				<li><? echo Html::anchor('dashboard', 'Dashboard'); ?></li>
-		    				<li><? echo Html::anchor('settings', 'Settings'); ?></li>
-		    				<li><? echo Html::anchor('logout', 'Logout'); ?></li>
+		    				<li><!-- <? echo Html::anchor('profile', 'View Profile'); ?> -->View Profile</li>
+		    				<li><!-- <? echo Html::anchor('dashboard', 'Dashboard'); ?> -->Dashboard</li>
+		    				<li><!-- <? echo Html::anchor('settings', 'Settings'); ?> -->Settings</li>
+		    				<li><!-- <? echo Html::anchor('logout', 'Logout'); ?> -->Logout</li>
 		    			</ul>
 		    	
 	    			</a>
--->
 
-					<? echo Html::anchor('dashboard', 'kmills006', array('class' => 'globe-nav')); echo Asset::img('profile_placeholders/kmills006_placeholder.png'); ?>
+<!-- 					<? echo Html::anchor('dashboard', 'kmills006', array('class' => 'globe-nav')); echo Asset::img('profile_placeholders/kmills006_placeholder.png'); ?> -->
 	    			
 	    			<a class="globe-nav" href="#"><? echo Asset::img('icons/new_notifications.png', array('width' => '30', 'height' => '21')); ?><span class="notifcation-count">18</span></a>
 	    			<a class="globe-nav" href="#">Browse</a>

8d92fce44a0ac9df168a5a5c28c6994e0bc21293
Kristy Miller <miller.kristy06@gmail.com>
2013-06-11 19:48:46 -0400

    [#51368563] - Achievements XP bar styles fixed

--- fuel/app/views/includes/footer.php
+++ fuel/app/views/includes/footer.php
@@ -1,17 +1,17 @@
     		<footer>
     			<div id="footer-sizer">
-    				<div class="site-nav">
+    				<nav class="site-nav">
 	    				<p>Site Navigation</p>
 	    			
 		    			<a href="signup.html">Create an Account</a>
 		    			<a href="#">Browse Decks</a>
 		    			<a href="#">Contact</a>
 		    			<a href="#">Help</a>
-    				</div>
+    				</nav>
     				
 	    			<p class="site-info">2sided is an online community that motivates users to study by allowing them to easily create and share flash card decks with classmates and friends, whether at home or on the go.</p>
 	    			
-	    			<p class="copyright">Copyright © 2sided, Kristy Miller</p>
+	    			<p class="copyright">Copyright &copy; 2sided, Kristy Miller</p>
     			</div>
     		</footer>
     	

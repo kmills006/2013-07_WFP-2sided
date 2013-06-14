20bf48b458284e080266c0602d7926690dc6b00b
Kristy Miller <miller.kristy06@gmail.com>
2013-06-14 16:39:27 -0400

    Username added to header on confirm new deck

--- fuel/app/classes/controller/deck.php
+++ fuel/app/classes/controller/deck.php
@@ -49,10 +49,10 @@ class Controller_Deck extends Controller_Template{
 		}
 
 
-
+		$data['username'] = Session::get('username');
 
 		$this->template->head    = View::forge('includes/head');
-		$this->template->header  = View::forge('includes/logged_in/header');
+		$this->template->header  = View::forge('includes/logged_in/header', $data);
 		$this->template->content = View::forge('deck/save');
 		$this->template->footer  = View::forge('includes/footer');
 	}

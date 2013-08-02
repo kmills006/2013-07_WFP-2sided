528546f1b1d687e59d2c208c763d47c1bf7148b8
Kristy Miller <miller.kristy06@gmail.com>
2013-08-01 23:19:16 -0400

    Bug fixed: path to check mark on user dashboard.

--- fuel/app/views/dashboard/index.php
+++ fuel/app/views/dashboard/index.php
@@ -79,7 +79,7 @@
 						  				</div>
 							  				
 							  			<div class="deck-social">
-							  				<p><img src="assets/img/icons/check_mark.png" alt="Rating Check Mark Icon" width="25" height="20"/></p>
+							  				<p><img src="../assets/img/icons/check_mark.png" alt="Rating Check Mark Icon" width="25" height="20"/></p>
 							  				<p><?= $deck->likes_count; ?></p>
 							  				<p><a href="#" class="share">Share Deck</a></p>
 							  			</div>
